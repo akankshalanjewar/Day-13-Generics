1ccc430477c73909b3ae45d141d5be9156283f9f
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-20 10:58:53 +0530

    [Akanksha]:checking maximum number between float value

---
+++ UC-2-MaximumNumberCheck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC_2_MaximumNumberCheck
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find max Number");
+            int output = MaximumNumber.MaximumIntegerNumber(89,22, 76);
+            Console.WriteLine(output);
+            double doubleoutput = MaximumNumber.MaximumFloatNumber(100.2, 44.45, 88.56);
+            Console.WriteLine(doubleoutput);
+        }
+    }
+}
